36e86f64c83f1499f50bd84727b7d01c3eb12198
yaroslav kobylsky <yaroslaff.k@gmail.com>
2017-09-19 16:02:13 +0300

    array test modified

--- src/main/scala/com/scalatest/test/ArrayTest.scala
+++ src/main/scala/com/scalatest/test/ArrayTest.scala
@@ -1,5 +1,7 @@
 package com.scalatest.test
 
+import scala.util.Sorting
+
 object ArrayTest {
   def main(args: Array[String]): Unit = {
     var myList = Array(1.9, 2.9, 3.4, 3.5);
@@ -21,6 +23,24 @@ object ArrayTest {
       }
       println();
     }
-  }
 
+    val array = Array(1,2,5,6,9,12,13,20);
+    println(array.mkString(" "));
+    println(array.sum);
+    println(array.min);
+    println(array.max);
+    println(array.sorted.reverse.mkString(" "));
+    Sorting.quickSort(array);
+    println(array.mkString(" "));
+    println(array.sorted.reverse(0));
+    println("devide by 2: " + array.count(_ % 2 == 0));
+
+    println();
+    println((for (element <- array) yield {2 * element;}).mkString(" "));
+    println((array.map(2 * _)).mkString(" "));
+    println();
+    println((for (element <- array if (element % 2 == 0)) yield {2 * element;}).mkString(" "));
+    println((array.filter(_ % 2 == 0).map(2 * _)).mkString(" "));
+
+  }
 }
